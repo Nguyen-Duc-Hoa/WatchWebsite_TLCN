b7ea23ea986b1b96bf044ed600f5d11006477666
yuhlee610 <yuhlee610@gmail.com>
2021-10-16 17:00:00 +0700

    create Constant class

---
+++ WatchWebsite_TLCN/WatchWebsite_TLCN/Utilities/Constant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchWebsite_TLCN.Utilities
+{
+    // Declare constant variables in this class
+    public static class Constant
+    {
+        public const string bestSelling = "Best Selling";
+        public const string priceDesending = "Price, high to low";
+        public const string priceAscending = "Price, low to high";
+        public const string alphabetically = "Alphabetically, A-Z";
+        public const string nonAlphabetically = "Alphabetically, Z-A";
+    }
+}
