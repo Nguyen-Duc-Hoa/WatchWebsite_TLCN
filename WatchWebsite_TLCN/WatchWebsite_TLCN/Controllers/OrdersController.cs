61cfdd84a2257ae4adf0dee4866c4dab7d6e64fd
Nguyễn Đức Hòa <72092427+Nguyen-Duc-Hoa@users.noreply.github.com>
2021-10-23 20:27:42 +0700

    api orders, orderdetails

--- WatchWebsite_TLCN/WatchWebsite_TLCN/Controllers/OrdersController.cs
+++ WatchWebsite_TLCN/WatchWebsite_TLCN/Controllers/OrdersController.cs
@@ -12,6 +12,7 @@ using Newtonsoft.Json;
 using WatchWebsite_TLCN.Models;
 using WatchWebsite_TLCN.DTO;
 using AutoMapper;
+using WatchWebsite_TLCN.Intefaces;
 
 namespace WatchWebsite_TLCN.Controllers
 {
@@ -21,11 +22,13 @@ namespace WatchWebsite_TLCN.Controllers
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly IUserOrder _userOrder;
 
-        public OrdersController(IUnitOfWork unitOfWork, IMapper mapper)
+        public OrdersController(IUnitOfWork unitOfWork, IMapper mapper, IUserOrder userOrder)
         {
             _mapper = mapper;
             _unitOfWork = unitOfWork;
+            _userOrder = userOrder;
         }
 
         // GET: api/Orders
@@ -191,5 +194,21 @@ namespace WatchWebsite_TLCN.Controllers
             return total*100;
         }
 
+
+        [HttpGet]
+        [Route("GetByUser/{userid}")]
+        public IEnumerable<Entities.Order> GetByUser(int userid)
+        {
+            var order = _userOrder.GetByUser(userid);
+            return order;
+        }
+
+        [HttpGet]
+        [Route("GetOrderDetail/{orderid}")]
+        public IEnumerable<OrderDetailDTO> GetOrderDetailr(int orderid)
+        {
+            var orderDetails = _userOrder.GetOrderDetails(orderid);
+            return orderDetails;
+        }
     }
 }
---
+++ WatchWebsite_TLCN/WatchWebsite_TLCN/Repository/UserOrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WatchWebsite_TLCN.DTO;
+using WatchWebsite_TLCN.Entities;
+using WatchWebsite_TLCN.Intefaces;
+
+namespace WatchWebsite_TLCN.Repository
+{
+    public class UserOrderRepository : IUserOrder
+    {
+        private readonly MyDBContext _context;
+        public UserOrderRepository(MyDBContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Order> GetByUser(int userid)
+        {
+            var order = _context.Orders.Where(x => x.UserId == userid && x.DeliveryStatus == "received").AsQueryable();
+            return order;
+        }
+
+        public IEnumerable<OrderDetailDTO> GetOrderDetails(int orderid)
+        {
+            var orderDetail = (from o in _context.OrderDetails
+                               join p in _context.Products on o.ProductId equals p.Id
+                               where o.OrderId == orderid
+                               select new OrderDetailDTO
+                               {
+                                   OrderId = o.OrderId,
+                                   ProductId = o.ProductId,
+                                   ProductName = o.ProductName,
+                                   Count = o.Count,
+                                   Price = o.Price,
+                                   Image = p.Image
+                               }).ToList();
+
+            return orderDetail;
+        }
+    }
+}
